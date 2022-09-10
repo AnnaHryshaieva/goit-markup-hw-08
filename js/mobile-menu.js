d63f52765e84fdbf6dfcf21917ed772101543ab2
AnnaHryshaieva <anet01073@gmail.com>
2022-09-11 01:48:11 +0300

    part3

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,22 +1,3 @@
-(() => {
-  const refs = {
-    openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
-    body: document.querySelector('body'),
-  };
-
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
-
-  function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
-  }
-})();
-
-//Закривається меню при переході на якір
-
 // (() => {
 //   const refs = {
 //     openMenuBtn: document.querySelector('[data-menu-open]'),
@@ -40,3 +21,18 @@
 //     refs.body.classList.remove('no-scroll');
 //   }
 // })();
+
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+  }
+})();
